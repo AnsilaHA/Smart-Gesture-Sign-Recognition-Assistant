f9ce75fa95c1c5e22e9912752fe104806d4cffaa
AnsilaHA <133792934+AnsilaHA@users.noreply.github.com>
2026-07-22 22:40:46 +0530

    Third Commit

--- gesture_model.py
+++ gesture_model.py
@@ -3,8 +3,6 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 import os
 
-
-
 DATASET_DIR = "dataset"
 
 def get_classes():
